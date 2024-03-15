be825737c055e8fbfd796f2e58808f03eb5819ac
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-15 13:11:13 -0700

    Add nullability support to template

--- src/Mycoshiro.Dataverse.LINQPad/CDSTemplatePartial.cs
+++ src/Mycoshiro.Dataverse.LINQPad/CDSTemplatePartial.cs
@@ -8,21 +8,32 @@ namespace Mycoshiro.Dataverse.LINQPad
     using EntityMetadataList = List<(EntityMetadata entityMetadata, List<(string attributeName, List<(string Label, int? Value)> options)> optionMetadata)>;
 
     [SuppressMessage("ReSharper", "InconsistentNaming")]
-    partial class CDSTemplate
+    public partial class CDSTemplate
     {
-        public string Namespace { get; init; }
+        [PublicAPI]
+        public string? Namespace { get; init; }
 
+        [PublicAPI]
         public string TypeName { get; set; }
 
         [PublicAPI]
         public EntityMetadataList Metadata { get; private set; }
 
+        [SuppressMessage("ReSharper", "ConvertToPrimaryConstructor")]
+        [SuppressMessage("Style", "IDE0290:Use primary constructor",
+            Justification = "Not supported on older .NET versions.")]
+        [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression",
+            Justification = "It is not unnecessary but rather ReSharper only.")]
+        [SuppressMessage("ReSharper", "UseCollectionExpression")]
         public CDSTemplate(
             EntityMetadataList? metadata = null,
             string? ns = null,
             string? typeName = null)
         {
-            Metadata = metadata ?? new();
+#pragma warning disable IDE0028
+            Metadata = metadata ?? new EntityMetadataList();
+#pragma warning restore IDE0028
+
             TypeName = typeName ?? "InvalidType";
             Namespace = ns ?? "InvalidNamespace";
         }
